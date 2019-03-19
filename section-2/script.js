b71ee69c33700263f009c7987b4b6360321e2992
Alexis Miguel Jimenez Olivares <alexis.m.jimenez.o@gmail.com>
2019-03-19 16:24:24 -0600

    :sparkles: Added comments on different parts of section.

--- section-2/script.js
+++ section-2/script.js
@@ -20,3 +20,89 @@ console.log(job)
 var _3years = 3
 var johnMark = 'John and Mark'
 // var if = 32
+
+console.log(firstName + ' ' + age)
+
+var a, b, c, d
+a = 'hola'
+b = 'tomas'
+d = false
+//Everything is coerced to string
+console.log(a + ' ' + b + ' ' + c + ' ' + d)
+
+// Javascript dynamic type
+b = 20
+console.log(b)
+
+// var lastName2 = prompt('Whats is this last name?')
+// console.log(lastName2)
+
+/****************************************
+ * Basic operators
+ */
+
+var year, yearJohn, yearkMark
+now = 2019
+ageJohn = 22
+ageMark = 25
+
+yearJohn = now - ageJohn
+yearMark = now - ageMark
+console.log(yearJohn)
+console.log(now + 2)
+console.log(now * 2)
+console.log(now / 2)
+
+//Logical operator
+var isJohnOlder = ageJohn > ageMark
+console.log(isJohnOlder)
+
+// typeof operator
+
+console.log(typeof isJohnOlder)
+console.log(typeof ageJohn)
+console.log(typeof 'a string')
+console.log(typeof z)
+
+/****************************************
+ * 3.Operatoror precedence
+ */
+
+var now3 = 2018
+var yearJohn = 1989
+var fullAge = 18
+
+var isFullAge = now3 - yearJohn >= fullAge
+console.log(isFullAge)
+
+var average = 10 + 100 / 2
+console.log(average)
+
+average = (10 + 100) / 2
+console.log(average)
+
+var sample1, sample2
+sample1 = sample2 = (3 + 5) * 4 - 6
+console.log(sample1, sample2)
+
+console.log(2 + 4 + 2 ** (2 ** 3) / 2 ? true === 3 < 2 : '0')
+
+// average = average * 2
+console.log(average)
+average *= 2
+console.log(average)
+
+average += 2
+console.log(average)
+
+average /= 2
+console.log(average)
+
+average = 2
+console.log(average)
+
+average **= 3
+console.log(average)
+
+average %= 5
+console.log(average)
