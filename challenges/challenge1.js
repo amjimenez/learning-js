783576fa022e7b45a272c9cf90d8960166430099
Alexis Jimenez <ajimenez@wiggot.com>
2019-03-19 16:32:48 -0600

    :sparkles: Finished first challenge.

---
+++ challenges/challenge1.js
@@ -0,0 +1,15 @@
+const john = {
+  mass: 44,
+  height: 1.7
+}
+const mark = {
+  mass: 80,
+  height: 1.5
+}
+
+mark.bmi = mark.mass / mark.height ** 2
+
+john.bmi = john.mass / john.height ** 2
+console.log(mark.bmi, john.bmi)
+const isMarkbmiGreater = mark.bmi > john.bmi
+console.log("Is Mark's bmi higher than John's? " + isMarkbmiGreater)
